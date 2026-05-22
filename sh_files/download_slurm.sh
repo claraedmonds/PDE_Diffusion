2c505a53ced0f02cf14179aac64c579cce1873ff
Clara Edmonds <claed@dtu.dk>
2026-05-22 14:42:22 +0200

    add slurm account

--- sh_files/download_slurm.sh
+++ sh_files/download_slurm.sh
@@ -3,6 +3,7 @@
 #SBATCH --output=logs/download/era5_%j.out
 #SBATCH --error=logs/download/era5_%j.err
 #SBATCH --partition=defq
+#SBATCH --account=iu_0104
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
